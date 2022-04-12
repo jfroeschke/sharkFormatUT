e3ce34055d64b453b30f509d72252a06067c8ae4
John Froeschke <john.froeschke@gmail.com>
2022-04-11 21:01:26 -0400

    Quick format of some data

---
+++ sharkFormatUT.R
@@ -0,0 +1,52 @@
+### Format Shark Data 
+### 4/11/2022
+
+library(doBy)
+library(reshape2)
+
+df <- read.csv("s2007.csv")
+
+##
+colnames(df)
+length(unique(df$decimalLatitude))
+length(unique(df$decimalLongitude))
+length(unique(df$vernacularName))
+###
+xy <- paste(df$decimalLatitude, df$decimalLongitude, sep=",")
+length(unique(xy))
+
+
+df2 <- summaryBy(vernacularName ~ decimalLongitude + decimalLatitude + genus +  specificEpithet, FUN=length, data=df,
+                 var.names="vernacularName")
+
+
+df3 <- summaryBy(genus +  specificEpithet ~ decimalLongitude + decimalLatitude + vernacularName, FUN=length, data=df,
+                 var.names="vernacularName")
+
+df4 <- dcast(df3, decimalLongitude + decimalLatitude ~ vernacularName, sum, value.var = "genus.length", fill=0)
+
+write.csv(df4, "s2007out.csv", row.names=FALSE)
+######## repeat for 2009
+
+df <- read.csv("s2009.csv")
+
+##
+colnames(df)
+length(unique(df$decimalLatitude))
+length(unique(df$decimalLongitude))
+length(unique(df$vernacularName))
+###
+xy <- paste(df$decimalLatitude, df$decimalLongitude, sep=",")
+length(unique(xy))
+
+
+df2 <- summaryBy(vernacularName ~ decimalLongitude + decimalLatitude + genus +  specificEpithet, FUN=length, data=df,
+                 var.names="vernacularName")
+
+
+df3 <- summaryBy(genus +  specificEpithet ~ decimalLongitude + decimalLatitude + vernacularName, FUN=length, data=df,
+                 var.names="vernacularName")
+
+df4 <- dcast(df3, decimalLongitude + decimalLatitude ~ vernacularName, sum, value.var = "genus.length", fill=0)
+
+write.csv(df4, "s2009out.csv", row.names=FALSE)
